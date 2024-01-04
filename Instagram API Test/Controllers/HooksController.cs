e2dc2a7bfc37e33d2fdca607ef0df7dbc9dc70c3
Tuğba Ergen <40305902+tugbargn@users.noreply.github.com>
2024-01-04 16:06:11 +0300

    Initial commit

---
+++ Instagram API Test/Controllers/HooksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HangWire.UI.Web.Controllers;
+
+[Route("hooks")]
+public class HooksController : Controller
+{
+  [HttpGet("instagram")]
+  public async Task<IActionResult> GetInstagramWebhook([FromQuery(Name = "hub.mode")] string mode, [FromQuery(Name = "hub.challenge")] int challenge, [FromQuery(Name = "hub.verify_token")] string verifyToken)
+  {
+    IActionResult actionResult;
+    try
+    {
+      actionResult = this.StatusCode(200, challenge);
+    }
+    catch (Exception exception)
+    {
+      actionResult = this.StatusCode(500, exception.Message);
+    }
+
+    return actionResult;
+  }
+
+}
+
